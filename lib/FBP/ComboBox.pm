20551cb72b4c9be7095799206cc0af3dad478b56
Adam Kennedy <adamk@cpan.org>
2010-07-07 17:24:35 +0000

    Import of ADAMK/FBP-0.07 from CPAN.
    gitpan-cpan-distribution: FBP
    gitpan-cpan-version:      0.07
    gitpan-cpan-path:         ADAMK/FBP-0.07.tar.gz
    gitpan-cpan-author:       ADAMK
    gitpan-cpan-maturity:     released

--- lib/FBP/ComboBox.pm
+++ lib/FBP/ComboBox.pm
@@ -2,7 +2,7 @@ package FBP::ComboBox;
 
 use Mouse;
 
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 
 extends 'FBP::Window';
 with    'FBP::Control';
--- lib/FBP/ListBox.pm
+++ lib/FBP/ListBox.pm
@@ -1,10 +1,11 @@
-package FBP::Dialog;
+package FBP::ListBox;
 
 use Mouse;
 
 our $VERSION = '0.07';
 
 extends 'FBP::Window';
+with    'FBP::Control';
 
 has style => (
 	is  => 'ro',
